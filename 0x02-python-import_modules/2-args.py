8b2cd462d3e163165b400daf743194851a4819e5
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-12-03 01:26:24 -0800

    Made corrections

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
         print("{} arguments.".format(lens))
     elif lens > 0:
         if lens == 1:
-            print("{} argument.".format(lens))
+            print("{} argument:".format(lens))
         else:
-            print("{} arguments.".format(lens))
+            print("{} arguments:".format(lens))
         for i in range(1, (lens + 1)):
             print("{}: {}".format(i, argv[i]))
