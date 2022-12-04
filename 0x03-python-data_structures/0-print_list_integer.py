69f860a3ac9c6e7c60697b99f56b05abaee38efb
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-12-04 14:47:10 -0800

    Made corrections

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+def int_to_str(i):
+    string = ''
+    while True:
+        i , remainder = divmod(i, 10)
+        string += chr(ord('0') + remainder)
+        if i == 0:
+            break
+    return string
+
+
 def print_list_integer(my_list=[]):
     lens = len(my_list)
     for i in range(lens):
-        print(str.format(f"{i}"))
+        val = int_to_str(i)
+        print(str.format(val))
