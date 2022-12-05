1b0b3ebbd5ea83f6654447d9e9843b9007a86d2e
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-12-05 14:42:19 -0800

    Made correction to 3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     lens = len(my_list) - 1
-    if lens <= 0:
+    if lens < 0:
         pass
     else:
         for i in range(lens, -1, -1):
