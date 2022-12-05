dbb75526c9394cdfcb36b1a2c2727e5c49d07cbc
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-12-04 16:23:14 -0800

    Added question 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    lens = len(my_list)
+    if lens < 0:
+        return my_list
+    elif idx >= lens:
+        return my_list
+    else:
+        new_list = my_list.copy()
+        for i in range(lens):
+            if i == idx:
+                new_list[i] = element
+                return new_list
