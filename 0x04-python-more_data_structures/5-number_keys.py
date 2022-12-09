14c25a01a0bc46f6ca03755c8cf3d3fb2776a2fb
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-12-09 14:41:28 -0800

    Added the solution of question 5

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """
+    This function returns the number of keys in a dictionary.
+    """
+    nb_keys = len(a_dictionary.keys())
+    return nb_keys
