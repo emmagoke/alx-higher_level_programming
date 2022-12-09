79ef35333e2fac5e79f584575f26139476113e12
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-12-09 11:41:20 -0800

    Made correction to 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,7 +2,7 @@
 def search_replace(my_list, search, replace):
     new_li = []
     for i in range(len(my_list)):
-        if my_list[i] ==  search:
+        if my_list[i] == search:
             new_li.append(replace)
         else:
             new_li.append(my_list[i])
