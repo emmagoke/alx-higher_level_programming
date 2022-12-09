8fc54a2bbaf231cdea2c68ac8f490615d863d3c6
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-12-09 11:37:44 -0800

    Added solution to question 1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_li = []
+    for i in range(len(my_list)):
+        if my_list[i] ==  search:
+            new_li.append(replace)
+        else:
+            new_li.append(my_list[i])
+    return new_li
