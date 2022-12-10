67bf31d0e3becb6994b8032c4dc9919c161fabd5
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-12-09 17:09:33 -0800

    Added the solution of question 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+    elif len(a_dictionary) == 0:
+        return None
+    else:
+        larg = sorted(a_dictionary.keys())[0]
+        for key in a_dictionary:
+            if a_dictionary[key] > a_dictionary[larg]:
+                larg = key
+    return larg
