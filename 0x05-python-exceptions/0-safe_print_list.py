3c5d75197eead68a1f888d94ce3898de015919fe
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-12-19 12:44:16 -0800

    Solved question 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    lens = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end='')
+            lens += 1
+    except:
+        lens += 0
+    finally:
+        print()
+        return lens
