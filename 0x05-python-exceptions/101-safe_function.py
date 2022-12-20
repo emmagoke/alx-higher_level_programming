4dcda9d06ab009072704d8c6c200a32a5d1b54ee
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-12-19 18:37:24 -0800

    Made corrections

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,7 +5,7 @@ import sys
 def safe_function(fct, *args):
     result = None
     try:
-        result = fct(args[0], args[1])
+        result = fct(*args)
     except Exception as error:
         sys.stderr.write("Exception: {}\n".format(error))
 
