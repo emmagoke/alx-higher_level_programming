6a51965f79ed3ae079b03bf074a68d217c2786e1
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-12-20 15:53:33 -0800

    Added question 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This Module contains the Square class
+"""
+
+
+class Square():
+    """
+    This is a Square class
+
+    Args:
+        size (integer): A private attribute
+    """
+    __size = 3
+
+    def __init__(self, size=0):
+        """
+        This creates an instance of the square class
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        Returns:
+            The area of the square of size
+        """
+        return (self.__size * self.__size)
