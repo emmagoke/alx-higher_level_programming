0b9d3c253c6cbea692eee944bbfd16f69204bbb8
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-01-10 16:15:54 -0800

    Added question 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,7 +2,8 @@
 """
 This module works with the command line
 """
-import os, sys
+import os
+import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
