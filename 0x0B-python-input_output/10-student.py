6cbea93ddc0fe42476b110575de9e60d89a13fe2
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-01-10 18:26:58 -0800

    Added question 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This module contains the Student class
+"""
+
+
+class Student:
+    """
+    This is the Student Class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        This method is called when an Instance of Student
+        is created.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        This method retrieves a dictionary representation
+        of a Student instance
+        """
+        if attrs == None:
+            out = self.__dict__
+        else:
+            out = {}
+            for key, value in self.__dict__.items():
+                if key in attrs:
+                    out[key] = value
+        return out
