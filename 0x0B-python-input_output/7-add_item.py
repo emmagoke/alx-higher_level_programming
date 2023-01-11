d42b85d03e3a59e1fb1d87e7efa3d678bf4bb8c7
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-01-10 16:14:36 -0800

    Added question 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module works with the command line
+"""
+import os, sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+cmd_line = sys.argv
+filename = 'add_item.json'
+content = []
+if filename in os.listdir():
+    content = load_from_json_file(filename)
+    content += cmd_line[1:]
+    save_to_json_file(content, filename)
+else:
+    content += cmd_line[1:]
+    save_to_json_file(content, filename)
