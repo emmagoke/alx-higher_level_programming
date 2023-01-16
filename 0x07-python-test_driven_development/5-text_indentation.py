2376f7fd4d8d9dd83cd130bd8687d4a0f022c3ee
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-01-16 04:06:48 -0800

    Added question 4

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+"""
+
+
+def text_indentation(text):
+    """
+    This function prints a text with 2 new lines
+    after each of these characters: ., ? and :
+    Arguments:
+        @text: The Text to be modify
+    """
+    if type(text) != str:
+        raise TypeError('text must be a string')
+
+    ind = 0
+    for word in text:
+        if word in '.?:':
+            print(word)
+            print()
+            ind += 1
+        elif text[ind - 1] in '.?:' and word == " ":
+            print("", end="")
+            ind += 1
+        else:
+            print(word, end="")
+            ind += 1
