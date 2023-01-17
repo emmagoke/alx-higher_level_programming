f18ef5e094c58a17ec6000844381d78e3e267d2e
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-01-17 13:16:58 -0800

    Added the question on roman numerial

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     # The dictionary of roman numerals
-    rom = {'I': 1, 'V':5, 'X':10,'L':50, 
-            'C':100,'D':500, 'M':1000}
+    rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+           'C': 100, 'D': 500, 'M': 1000}
     result = 0
     if type(roman_string) != str or roman_string is None:
         return result
-    
+
     lens = len(roman_string)
 
     for num in range(lens):
