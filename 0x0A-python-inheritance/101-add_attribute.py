69b8748a82017eed6781606e020dab3f82447863
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-02-01 18:55:57 -0800

    Added question 13

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
-
+"""
+This module contains the add_attribute function
+which checks if a class has the attribute
+"""
 def add_attribute(cls, attr, value):
     """
+    This function checks if attr is an attribute
+    of cls
+    args:
+        cls: class
+        attr: attribute
+        value: value
     """
     if hasattr(cls, attr):
         raise TypeError("can't add new attribute")
