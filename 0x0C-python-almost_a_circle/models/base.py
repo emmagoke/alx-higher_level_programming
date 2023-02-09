f49dfe538ef00740e221cf6cecafbead4501ff0d
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-02-09 01:35:41 -0800

    Added question 17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -47,3 +47,13 @@ class Base:
                 output.append(obj.to_dictionary())
         with open(name + '.json', mode='w', encoding='utf-8') as f:
             f.write(cls.to_json_string(output))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        returns the list of the JSON string representation json_string
+        """
+        if json_string is None:
+            return []
+        else:
+            return json.loads(json_string)
