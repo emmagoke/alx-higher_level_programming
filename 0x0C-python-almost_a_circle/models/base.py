89a4adf3a9d2329309c679a4d3571133ed81078c
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-02-09 01:21:14 -0800

    Added question 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -31,3 +31,21 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        This function rites the JSON string representation of
+        list_objs to a file.
+        """
+        output = []
+        name = ''
+        if list_objs is None:
+            output = '[]'
+            name = cls.__class__.__name__
+        else:
+            name = list_objs[0].__class__.__name__
+            for obj in list_objs:
+                output.append(obj.to_dictionary())
+        with open(name + '.json', mode='w', encoding='utf-8') as f:
+            f.write(cls.to_json_string(output))
