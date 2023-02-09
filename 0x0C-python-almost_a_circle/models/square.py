e522c20100eae184c6fc08bd0c0e608250a793db
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-02-08 18:11:16 -0800

    Added question 10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,32 @@
+#!/usr/binpython3
+"""
+This Module contains the python class Square
+that inherits from the Rectangle class
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    The Square class that has following attribute:
+
+    attributes:
+        size
+        x
+        y
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        This method is called when the instance of the 
+        class square is created.
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        This method returns the string representation
+        of the instance of this class
+        """
+        args = (self.id, self.x, self.y, self.width)
+        return "[Square] ({}) {}/{} - {}".format(*args)
