6aaa40ebc27c91c4bccdbf9e1513dd00f9f524f9
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-02-09 00:49:11 -0800

    Added question 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 This module contains the Base Class
 """
+import json
 
 
 class Base:
@@ -20,3 +21,13 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        returns the JSON string representation of list_dictionaries
+        """
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
