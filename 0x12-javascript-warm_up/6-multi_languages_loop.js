93d411f3a182f9ccc4196f7ab071d63a6e75bed3
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-03-13 02:47:04 -0700

    Added question 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const {argv} = require('node:process');
+
+const input = ["C is fun", "Python is cool", "JavaScript is amazing"]
+
+input.forEach((val) => {
+	console.log(`${val}`);
+})
