3c70a51ca32874e130c7faa8405ad469bc808a73
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-03-13 05:04:24 -0700

    Made corrections to question 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const first = "C is fun"
-const second = "Python is cool"
-const third = "JavaScript is amazing"
+const first = 'C is fun'
+const second = 'Python is cool'
+const third = 'JavaScript is amazing'
 
-console.log(first)
-console.log(second)
-console.log(third)
+console.log(first);
+console.log(second);
+console.log(third);
