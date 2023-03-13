a93d64277a53d586d48658d0e34d637f21fc89a9
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-03-12 17:20:28 -0700

    Added question 15

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def complex_delete(a_dictionary, value):
+    """
+    This function deletes keys with a specific value
+    in a dictionary.
+    """
+    key_ = []
+    for key, val in a_dictionary.items():
+        if val == value:
+            key_.append(key)
+    if key_:
+        for key in key_: 
+            del a_dictionary[key]
+    return a_dictionary
