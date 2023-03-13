2878066855601b792c0ba1b3f75fa2313e9ee352
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-03-13 06:08:21 -0700

    Corrections to question 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-
+/*
 const { argv } = require('node:process');
 
 const inputarr = [];
@@ -7,10 +7,12 @@ const inputarr = [];
 for (let i = 2; i < argv.length; i++) {
   inputarr.push(argv[i]);
 }
+*/
+const length = process.argv.length;
 
-if (inputarr.length === 0) {
+if (length < 3) {
   console.log('No argument');
-} else if (inputarr.length === 1) {
+} else if (length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
