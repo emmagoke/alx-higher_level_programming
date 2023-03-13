04520ca29d2ecaa55581a867db9c4cdaff50297e
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-03-13 06:12:05 -0700

    Made corrections to 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
 
-const {argv} = require('node:process');
+/*const { argv } = require('node:process');*/
 
-if (argv[2])
-{
-	console.log(argv[2]);
-}
-else
-{
-	console.log("No argument");
+if (process.argv[2]) {
+  console.log(process.argv[2]);
+} else {
+  console.log('No argument');
 }
