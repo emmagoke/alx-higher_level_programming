eedb5e2749da26662c5f8c75bffce20da8a2cfc1
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-03-13 02:47:35 -0700

    Added question 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const {argv} = require('node:process');
+
+if (Number.isNaN(parseInt(argv[2])))
+{
+	console.log("Missing number of occurrences");
+}
+else
+{
+	for (let i = 0; i < parseInt(argv[2]); i++)
+	{
+		console.log("C is fun");
+	}
+}
