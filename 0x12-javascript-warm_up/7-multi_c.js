498b3e0c2a80dfe34e5fa6d8f7f99787d48a2891
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-03-13 06:24:57 -0700

    Made corrections to 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,15 +1,11 @@
 #!/usr/bin/node
 
-const {argv} = require('node:process');
+/* const { argv } = require('node:process'); */
 
-if (Number.isNaN(parseInt(argv[2])))
-{
-	console.log("Missing number of occurrences");
-}
-else
-{
-	for (let i = 0; i < parseInt(argv[2]); i++)
-	{
-		console.log("C is fun");
-	}
+if (Number.isNaN(parseInt(process.argv[2]))) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log('C is fun');
+  }
 }
