cfe6c97b718ea2653eaaffb19d13064e3fd1d5a3
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-03-14 05:38:54 -0700

    Added question 3

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,8 +2,7 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w === 0 || h === 0 || w < 0 || h < 0) {
-    } else {
+    if ((w > 0) && (h > 0)) {
       this.width = w;
       this.height = h;
     }
