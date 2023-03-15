3e5fd351a5e01493be970126f68f33d7f5507c84
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-03-15 16:48:09 -0700

    Added question 9

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 let args = 0;
 
+/* This function prints the number of arguments
+ * already printed and the new argument value.
+ */
 exports.logMe = function (item) {
   console.log(`${args}: ${item}`);
   args += 1;
