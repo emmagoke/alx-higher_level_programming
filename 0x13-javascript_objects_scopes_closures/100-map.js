9240b0c639271a7cef394256b2e39b8ec4cb6537
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-03-15 18:03:08 -0700

    Added question 11

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,11 +1,6 @@
 #!/usr/bin/node
 
 const list = require('./100-main').list;
-/* This function imports an array and computes a
- * new array. The new array is equal to equal to the
- * value of the initial list, multipled by the index
- * in the list.
- */
 
 console.log(list);
 console.log(list.map((item, index) => item * index));
