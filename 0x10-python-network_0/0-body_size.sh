5020f03336d8d73701cd313c1e1c7a5244354d1b
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-04-04 10:45:40 -0700

    Added question 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 # Write a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-
-url=$1
-
-curl -sI -w '%{size_header}' "$url" | tail -1; echo ""
+curl -sI -w '%{size_header}' "$1" | tail -1; echo ""
