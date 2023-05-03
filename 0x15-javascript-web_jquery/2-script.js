6d97890f7a99acf87515d199e83348ab9946bbef
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-05-03 16:56:55 -0700

    Added task 2

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,9 @@
+/**
+ * This script updates the text color of the <header> element to red (#FF0000)
+ * when the user clicks on the tag DIV#red_header
+ */
+$('DIV#red_header').bind({
+  click: function () {
+    $('header').css('color', '#FF0000');
+  }
+});
