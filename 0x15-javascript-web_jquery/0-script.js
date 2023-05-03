c979dd51949f64fb294e404b8f65f5f7fd427358
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-05-03 16:27:21 -0700

    Added task 1

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,5 +1,5 @@
 /**
  * This script changes the colour of the content of the header tag
  */
-let header = document.querySelector("header");
-header.style.color = "#FF0000";
+const header = document.querySelector('header');
+header.style.color = '#FF0000';
